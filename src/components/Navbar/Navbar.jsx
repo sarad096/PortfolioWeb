a583e162f4b3caba7fcc026ec4afae3d612136e8
sarad096 <saradgyawali096@gmail.com>
2022-06-28 13:34:28 +0545

    Added Hamburger Menu For Responsive Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     }
     window.addEventListener('scroll',changeBackground);
 
-
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
 
 
 
@@ -28,7 +28,26 @@ const Navbar = () => {
   return (
     <div>
 <header className={navbar?'navbar active':'navbar'}>
-    <nav>
+<button className="hamburger"
+onClick={() => {
+    setIsNavExpanded(!isNavExpanded);
+  }}
+>
+        
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+    <nav className= {isNavExpanded ? "navigation-menu expanded" : "navigation-menu"} >
         <ul className="nav-links">
             <li >
                 
