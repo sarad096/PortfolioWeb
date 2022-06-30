b5c6f40234e84fd570244847c2f345f9537889ff
sarad096 <saradgyawali096@gmail.com>
2022-06-30 08:32:41 +0545

    Added Active Link And onClick function In Projects Button

--- src/components/projects/Card/Card.jsx
+++ src/components/projects/Card/Card.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 const Card = (props) => {
+
+ 
   return (
     <div className="card">
       <div className="card-body">
         <h2 className="card-title">{props.title}</h2>
         <p className="card-desc">{props.desc}</p>
       </div>
-      <button className="card-btn">
+      <button className="card-btn" onClick={()=>{
+        window.open(props.url,'_blank')
+      }}>
         <a href={props.url} target="_blank" rel="noreferrer">
-          {" "}
           Check Out
         </a>
       </button>
